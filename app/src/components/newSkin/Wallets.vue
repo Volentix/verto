22787734d76a97d93400712b77fef2a551befdd6
youssef bitar <bitardev@gmail.com>
2020-02-27 14:29:40 +0100

    New skin ui

--- app/src/components/newSkin/Wallets.vue
+++ app/src/components/newSkin/Wallets.vue
@@ -2,8 +2,8 @@
 <div>
   <div class="wallets-wrapper">
     <!-- <q-toggle v-model="active" label="Active" /> -->
-    <div class="wallets-wrapper--list">
-      <q-list bordered separator>
+    <div class="wallets-wrapper--list" :class="{'open': showWallet}">
+      <q-list bordered separator class="list-wrapper">
         <q-item v-for="(item, index) in menu" :key="index" clickable v-ripple :active="active" :to="item.to">
           <q-item-section avatar>
             <img class="coin-icon" width="35px" :src="item.icon" alt="">
@@ -18,7 +18,7 @@
           </q-item-section>
         </q-item>
       </q-list>
-       <q-btn unelevated flat icon-right="keyboard_arrow_down" class="full-width wallets-wrapper--list__hide-wallets" color="white" text-color="black" label="Hide all wallets" />
+       <q-btn @click="toggleWallets()" unelevated flat :icon-right="showText ? 'keyboard_arrow_up': 'keyboard_arrow_down'" class="full-width wallets-wrapper--list__hide-wallets" color="white" text-color="black" :label="showText ? 'Hide all wallets' : 'Show all wallets'" />
     </div>
   </div>
 </div>
@@ -30,6 +30,8 @@ export default {
   data () {
     return {
       active: true,
+      showWallet: false,
+      showText: false,
       menu: [
         { name: 'BTC xyz', purcent: '1.02%', to: '/wallets', icon: 'statics/coins_icons/btc.png', amount: '0.023 BTC', amountUSD: '$235.21' },
         { name: 'VTX', purcent: '1.02%', to: '/wallets', icon: 'statics/coins_icons/vtx.png', amount: '0.023 BTC', amountUSD: '$235.21' },
@@ -40,6 +42,10 @@ export default {
     }
   },
   methods: {
+    toggleWallets () {
+      this.showWallet = !this.showWallet
+      this.showText = !this.showText
+    }
   }
 }
 </script>
@@ -54,9 +60,28 @@ export default {
         box-shadow: 0px 3px 6px 0px rgba(black, .19);
         &__hide-wallets{
           text-transform: initial !important;
-          margin-top: 20px;
-          margin-bottom: 0px;
+          margin-top: 0px;
+          margin-bottom: 10px;
           color: #7272FA !important;
+        }
+        .list-wrapper{
+          overflow: hidden;
+          visibility: hidden;
+          height: 0px;
+          opacity: 0;
+          transform: translateY(-20px) scaleY(.5);
+          transform-origin: top;
+          transition: ease transform .3s, ease opacity .4s;
+        }
+        &.open{
+          margin-bottom: -100px;
+          .list-wrapper{
+            visibility: visible;
+            height: auto;
+            opacity: 1;
+            transform: translateY(0px) scaleY(1);
+            margin-bottom: 40px;
+          }
         }
         /deep/ .q-list--bordered {
             border: none;
