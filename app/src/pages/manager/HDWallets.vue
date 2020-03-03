45c4ce7c81c40b24f3e18793334cb5d1632d9d84
Gregory Luneau <gregory.luneau@gmail.com>
2020-03-02 21:21:42 -0500

    dashboard progress

--- app/src/pages/manager/HDWallets.vue
+++ app/src/pages/manager/HDWallets.vue
@@ -1,25 +1,49 @@
 <template>
-  <q-page padding>
+  <q-page padding class="flex flex-center text-white bg-black">
     <q-card flat class="bg-black text-center" style="width: 100%; max-width: 700px;">
     <q-stepper active-color="green" done-color="green" ref="stepper" alternative-labels animated v-model="step">
       <q-step default :name="1" :done="step>1" title="Enable new Wallet" class=" bg-black workflow-step">
         <q-card-section>
-          <q-list dark bordered separator>
-            <q-item v-for="(item, index) in assets" :key="index">
-              <q-item-section avatar>
-                <q-avatar>
-                  <img :src="getImages(item.symbol)">
-                </q-avatar>
-              </q-item-section>
-              <q-item-section class="col-2">
-                <q-item-label>{{ item.name }}</q-item-label>
-                <q-item-label caption>{{ item.symbol }}</q-item-label>
-              </q-item-section>
-              <q-item-section>
-                <q-toggle dark color="green" v-model="item.enabled" :disable="item.inconfig" />
-              </q-item-section>
-            </q-item>
-          </q-list>
+          <q-select
+            dark
+            label="Select Chain to add"
+            separator
+            v-model="selected"
+            :options="assets"
+          >
+            <template v-slot:option="scope">
+              <q-item
+                class="custom-menu"
+                v-bind="scope.itemProps"
+                v-on="scope.itemEvents"
+              >
+                <q-item-section avatar>
+                  <q-icon :name="`img:${getImages(scope.opt.value)}`" />
+                </q-item-section>
+                <q-item-section dark>
+                  <q-item-label v-html="scope.opt.label" />
+                  <q-item-label caption>{{ scope.opt.value }}</q-item-label>
+                </q-item-section>
+              </q-item>
+            </template>
+            <template v-slot:selected>
+              <q-item
+                v-if="selected"
+              >
+                <q-item-section avatar>
+                  <q-icon :name="`img:${getImages(selected.value)}`" />
+                </q-item-section>
+                <q-item-section dark>
+                  <q-item-label v-html="selected.label" />
+                  <q-item-label caption>{{ selected.value }}</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item
+                v-else>
+              </q-item>
+            </template>
+          </q-select>
+
           <div class="q-pa-sm" @click="step=2" >
             <q-icon name="navigate_next" size="3.2rem" color="green"   >
               <q-tooltip>{{ $t('next') }}</q-tooltip>
@@ -77,26 +101,29 @@ export default {
       isPwd: true,
       goodPassword: false,
       vertoPassword: null,
-      assets: {
-        'verto': { 'symbol': 'EOS', 'name': 'EOS' },
-        'eos': { 'symbol': 'EOS', 'name': 'EOS' },
-        'btc': { 'symbol': 'BTC', 'name': 'Bitcoin' },
-        'eth': { 'symbol': 'ETH', 'name': 'Ethereum' },
-        'bnb': { 'symbol': 'BNB', 'name': 'Binance Coin' },
-        'ltc': { 'symbol': 'LTC', 'name': 'Litecoin' },
-        'dash': { 'symbol': 'DASH', 'name': 'DASH' },
-        'xrp': { 'symbol': 'XRP', 'name': 'Ripple' },
-        'xlm': { 'symbol': 'XLM', 'name': 'Stellar Lumens' },
-        'xtz': { 'symbol': 'XTZ', 'name': 'Tezos' },
-        'ada': { 'symbol': 'ADA', 'name': 'Cardano' } }
+      selected: null,
+      assets: [
+        { 'value': 'verto', 'label': 'EOS' },
+        { 'value': 'eos', 'label': 'EOS' },
+        { 'value': 'btc', 'label': 'Bitcoin' },
+        { 'value': 'eth', 'label': 'Ethereum' },
+        { 'value': 'bnb', 'label': 'Binance Coin' },
+        { 'value': 'ltc', 'label': 'Litecoin' },
+        { 'value': 'dash', 'label': 'DASH' },
+        { 'value': 'xrp', 'label': 'Ripple' },
+        { 'value': 'xlm', 'label': 'Stellar Lumens' },
+        { 'value': 'xtz', 'label': 'Tezos' },
+        { 'value': 'ada', 'label': 'Cardano' } ]
     }
   },
   async created () {
     const self = this
     this.$store.state.currentwallet.config.keys.forEach(key => {
       if (key.origin === 'mnemonic') {
-        self.assets[key.type].enabled = true
-        self.assets[key.type].inconfig = true
+        self.assets = self.assets.filter(function (type) {
+          return type.value !== key.type
+        })
+
         console.log('assets', self.assets)
       }
     })
@@ -107,14 +134,10 @@ export default {
   },
   methods: {
     async enable () {
-      Object.keys(this.assets).forEach(async symbol => {
-        if (this.assets[symbol].enabled && !this.assets[symbol].inconfig) {
-          console.log('assets', this.assets[symbol])
-          const keys = await HD.Wallet(symbol)
-          const result = await this.$configManager.saveWalletAndKey(this.assets[symbol].name, this.vertoPassword, null, keys.publicKey, keys.privateKey, symbol, 'mnemonic')
-          console.log(result)
-        }
-      })
+      const keys = await HD.Wallet(this.selected.value)
+      const result = await this.$configManager.saveWalletAndKey(this.selected.label, this.vertoPassword, null, keys.publicKey, keys.privateKey, this.selected.value, 'mnemonic')
+      console.log(result)
+
       this.$router.push({ path: '/wallet' })
     },
     async checkVertoPassword () {
@@ -133,9 +156,21 @@ export default {
       // })
     },
     getImages (symbol) {
-      return 'https://files.coinswitch.co/public/coins/' + symbol.toLowerCase() + '.png'
+      return 'https://files.coinswitch.co/public/coins/' + symbol + '.png'
     }
   }
 }
 
 </script>
+
+<style lang="stylus" scoped>
+.custom-menu {
+  background: #424242
+}
+.q-item__label {
+  color: #ffffff;
+}
+.q-item__label--caption {
+  color: #848484;
+}
+</style>
