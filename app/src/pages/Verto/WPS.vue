1f77d5afb66a3e1d169be777948c50892f108744
Avral <avral@pm.me>
2020-04-15 01:10:47 +0800

    WPS alpha version

--- app/src/pages/Verto/WPS.vue
+++ app/src/pages/Verto/WPS.vue
@@ -1,184 +1,100 @@
 <template>
-  <q-page class="text-black bg-white">
-    <div class="standard-content">
-      <h2 class="standard-content--title flex justify-center">
-        <q-btn flat unelevated class="btn-align-left" to="/verto/dashboard" text-color="black" icon="keyboard_backspace" />
+  <q-page class='text-black bg-white'>
+    <div class='standard-content'>
+      <h2 class='standard-content--title flex justify-center'>
+        <q-btn flat unelevated class='btn-align-left' to='/verto/dashboard' text-color='black' icon='keyboard_backspace' />
         Worker proposals
       </h2>
-      <div class="standard-content--body">
-        <div class="standard-content--body__form">
-          <span class="lab-input">From</span>
-          <q-select
-              light
-              separator
-              rounded
-              outlined
-              class="select-input"
-              v-model="fromCoin"
-              use-input
-              :options="optionsFrom"
-          >
-            <template v-slot:option="scope">
-              <q-item
-                class="custom-menu"
-                v-bind="scope.itemProps"
-                v-on="scope.itemEvents"
-              >
-                <q-item-section avatar>
-                  <q-icon class="option--avatar option--avatar" :class="scope.opt.value" :name="`img:${scope.opt.image}`" />
-                </q-item-section>
-                <q-item-section dark>
-                  <q-item-label v-html="scope.opt.label" />
-                  <q-item-label caption>{{ scope.opt.value }}</q-item-label>
-                </q-item-section>
-              </q-item>
-            </template>
-            <template v-slot:selected>
-              <q-item
-                v-if="fromCoin"
-              >
-                <q-item-section avatar>
-                  <q-icon class="option--avatar option--avatar__custom" :class="fromCoin.value" :name="`img:${fromCoin.image}`" />
-                </q-item-section>
-                <q-item-section>
-                  <q-item-label v-html="fromCoin.label" />
-                  <q-item-label caption>{{ fromCoin.value }}</q-item-label>
-                </q-item-section>
-              </q-item>
-              <q-item
-                v-else>
-              </q-item>
-            </template>
-          </q-select>
-          <br>
-          <span class="lab-input">To</span>
-          <q-select
-              light
-              separator
-              rounded
-              outlined
-              class="select-input"
-              v-model="toCoin"
-              use-input
-              :options="optionsTo"
-          >
-            <template v-slot:option="scope">
-              <q-item
-                class="custom-menu"
-                v-bind="scope.itemProps"
-                v-on="scope.itemEvents"
-              >
-                <q-item-section avatar>
-                  <q-icon class="option--avatar option--avatar" :class="scope.opt.value" :name="`img:${scope.opt.image}`" />
-                </q-item-section>
-                <q-item-section dark>
-                  <q-item-label v-html="scope.opt.label" />
-                  <q-item-label caption>{{ scope.opt.value }}</q-item-label>
-                </q-item-section>
-              </q-item>
-            </template>
-            <template v-slot:selected>
-              <q-item
-                v-if="toCoin"
-              >
-                <q-item-section avatar>
-                  <q-icon class="option--avatar option--avatar__custom" :class="toCoin.value" :name="`img:${toCoin.image}`" />
-                </q-item-section>
-                <q-item-section>
-                  <q-item-label v-html="toCoin.label" />
-                  <q-item-label caption>{{ toCoin.value }}</q-item-label>
-                </q-item-section>
-              </q-item>
-              <q-item
-                v-else>
-              </q-item>
-            </template>
-          </q-select>
-          <br>
-          <div class="pay-get-wrapper flex justify-between item-center content-center">
-            <div class="pay-wrapper column flex-center">
-              <span class="label">you pay</span>
-              <span class="value">0 EOS</span>
-            </div>
-            <q-btn flat unelevated class="exchange-btn" text-color="black">
-              <q-icon name="keyboard_backspace" class="left-icon" />
-              <q-icon name="keyboard_backspace" class="right-icon" />
-            </q-btn>
-            <div class="get-wrapper column flex-center">
-              <span class="label">you get</span>
-              <span class="value">0 VTX</span>
-            </div>
-          </div>
-        </div>
+
+      <q-btn @click="refresh"> Refresh </q-btn>
+
+      <h5> Active proposals: </h5>
+
+      <div v-for="proposal in proposals" :key="proposal.proposal_name">
+        <p> {{ proposal.title }} </p>
+        <p> proposer: {{ proposal.proposer }} </p>
+        <p> budget: {{ proposal.monthly_budget }} ( duration: {{ proposal.duration }} ) </p>
+        <span>
+          {{ proposal.created }}
+        </span>
+        <span>
+          JSON: {{ proposal.proposal_json }}
+        </span>
+
         <br>
-        <div class="rate-value flex justify-center">
-          <span class="label">Rate VTX=</span>
-          <span class="value"> 0.03254 XRP</span>
-        </div>
-        <br>
-        <div class="standard-content--footer">
-          <q-btn flat class="action-link next" color="black" text-color="white">
-            <span class="label">Exchange EOS <q-icon name="keyboard_backspace" color="white" class="left-icon" /> VTX</span>
-          </q-btn>
-        </div>
-        <div class="progress-custom-volentix column flex-center">
-          <svg class="svg_logo" fill="#7272FA" width="40" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 20.58"><path d="M199,25.24q0,3.29,0,6.57a.5.5,0,0,1-.18.41l-7.32,6.45a.57.57,0,0,1-.71,0l-7.21-6.1c-.12-.11-.25-.22-.38-.32a.53.53,0,0,1-.22-.47q0-3.83,0-7.66,0-2.69,0-5.39c0-.33.08-.47.29-.51s.33.07.44.37l3.45,8.84c.52,1.33,1,2.65,1.56,4a.21.21,0,0,0,.23.16h4.26a.19.19,0,0,0,.21-.14l3.64-9.7,1.21-3.22c.08-.22.24-.32.42-.29a.34.34,0,0,1,.27.37c0,.41,0,.81,0,1.22Q199,22.53,199,25.24Zm-8.75,12s0,0,0,0,0,0,0,0a.28.28,0,0,0,0-.05l-1.88-4.83c0-.11-.11-.11-.2-.11h-3.69s-.1,0-.13,0l.11.09,4.48,3.8C189.38,36.55,189.8,36.93,190.25,37.27Zm-6.51-16.76h0s0,.07,0,.1q0,5.4,0,10.79c0,.11,0,.16.15.16h4.06c.15,0,.15,0,.1-.16s-.17-.44-.26-.66l-3.1-7.94Zm14.57.06c-.06,0-.06.07-.07.1l-1.89,5q-1.06,2.83-2.13,5.66c-.06.16,0,.19.13.19h3.77c.16,0,.2,0,.2-.2q0-5.3,0-10.59Zm-7.16,17,.05-.11,1.89-5c.05-.13,0-.15-.11-.15h-3.71c-.17,0-.16,0-.11.18.26.65.51,1.31.77,2Zm.87-.3,0,0,5.65-5H194c-.13,0-.16.07-.19.17l-1.59,4.23Zm0,.06h0Z" transform="translate(-183 -18.21)"></path></svg>
-          <span class="title">Processing</span>
-          <q-linear-progress rounded size="md" :value="progress" class="q-mt-md" />
-        </div>
+        <q-btn outline @click="claim(proposal.proposal_name)"> Claim </q-btn>
+        <span> | </span>
+        <q-btn outline @click="vote(proposal.proposal_name, 'yes')"> Vote(Yes) </q-btn>
+        <span> | </span>
+        <q-btn outline @click="vote(proposal.proposal_name, 'no')"> Vote(No) </q-btn>
+
+        <hr>
+      </div>
+
+      <h5> My draft proposals: </h5>
+
+      <div v-for="proposal in drafts" :key="proposal.proposal_name">
+        <p> {{ proposal.title }} </p>
+        <p> proposer: {{ proposal.proposer }} </p>
+        <p> budget: {{ proposal.monthly_budget }} ( duration: {{ proposal.duration }} ) </p>
+
+        <q-btn
+          outline
+          @click="activate(proposal.proposal_name)"
+        >
+          Activate
+        </q-btn>
+
+        <span> | </span>
+
+        <q-btn
+          outline
+          @click="canceldraft(proposal.proposal_name)"
+        >
+          Cancel
+        </q-btn>
+
+        <hr>
+
+        <h5> Create draft proposal </h5>
+
+        <q-input v-model="proposal_name" color="deep-purple-14" label="Proposal name (id)" />
+        <q-input v-model="title" color="deep-purple-14" label="Proposal Titile" />
+        <q-input v-model="monthly_budget" color="deep-purple-14" label="Budget" />
+        <q-input v-model="duration" color="deep-purple-14" label="Duration" />
+        <q-input v-model="proposal_json" color="deep-purple-14" label="Proposal json" value="asdfasdf"/>
+
+        <br/>
+        <q-btn @click="submitdraft" outline > Sumbit draft </q-btn>
       </div>
     </div>
   </q-page>
 </template>
 
 <script>
-import configManager from '@/util/ConfigManager'
+/* eslint-disable */ // FIXME camelcase
+
 import { version } from '../../../package.json'
 import EosWrapper from '@/util/EosWrapper'
+import { userError } from '@/util/errorHandler'
 
 const eos = new EosWrapper()
 let platformTools = require('../../util/platformTools')
 if (platformTools.default) platformTools = platformTools.default
 
+
 export default {
   components: {},
   data () {
     return {
-      fromCoin: null,
-      toCoin: null,
-      progress: 0.2,
-      to: '',
-      amount: '',
-      memo: '',
-      optionsFrom: [
-        {
-          label: 'Ethereum',
-          value: 'eth',
-          image: 'https://files.coinswitch.co/public/coins/eth.png'
-        },
-        {
-          label: 'Bitcoin',
-          value: 'btc',
-          image: 'https://files.coinswitch.co/public/coins/btc.png'
-        }
-      ],
-      optionsTo: [
-        {
-          label: 'Volentix',
-          value: 'vtx',
-          image: 'statics/coins_icons/svg_logo_white.svg'
-        },
-        {
-          label: 'Bitcoin',
-          value: 'btc',
-          image: 'https://files.coinswitch.co/public/coins/btc.png'
-        }
-      ],
-      minimizedModal: false,
-      message: '',
-      version: {},
-      network: this.$store.state.settings.network,
-      configPath: ''
+      proposals: [],
+      drafts: [],
+
+      proposal_name: 'mywps',
+      title: 'My WPS Title',
+      monthly_budget: '8.00000000 VTX',
+      duration: '2',
+      proposal_json: '[{"key":"somedata", "value":"text data here"}]'
     }
   },
   mounted () {
@@ -187,38 +103,173 @@ export default {
   },
 
   async created () {
-    const result = await eos.getTable(
-      'volentixwork', 'volentixwork', 'proposals'
-    )
-    console.log(result)
+    this.fetch()
   },
 
   methods: {
+    async vote (proposal_name, vote) {
+      try {
+        await eos.transact({
+          actions: [{
+            account: 'volentixwork',
+            name: 'vote',
+            authorization: [{
+              actor: this.walletName,
+              permission: 'active'
+            }],
+            data: { voter: this.walletName, proposal_name, vote }
+          }]
+        })
+        this.fetch()
+      } catch (error) {
+        console.log('err', error)
+        userError(error.message)
+      }
+    },
+
+    async claim (proposal_name) {
+      try {
+        await eos.transact({
+          actions: [{
+            account: 'volentixwork',
+            name: 'claim',
+            authorization: [{
+              actor: this.walletName,
+              permission: 'active'
+            }],
+            data: { proposal_name }
+          }]
+        })
+        this.fetch()
+      } catch (error) {
+        console.log('err', error)
+        userError(error.message)
+      }
+    },
+
+    async refresh () {
+      try {
+        await eos.transact({
+          actions: [{
+            account: 'volentixwork',
+            name: 'refresh',
+            authorization: [{
+              actor: this.walletName,
+              permission: 'active'
+            }],
+            data: {}
+          }]
+        })
+        this.fetch()
+      } catch (error) {
+        console.log('err', error)
+        userError(error.message)
+      }
+    },
+    async submitdraft () {
+      const privateKeyEncrypted = JSON.stringify(this.$store.state.currentwallet.wallet.privateKeyEncrypted)
+      const privateKey = this.$configManager.decryptPrivateKey(this.privateKeyPassword, privateKeyEncrypted)
+
+      const { proposal_name, title, monthly_budget, duration, proposal_json } = this
+
+      try {
+        await eos.transact({
+          actions: [{
+            account: 'volentixwork',
+            name: 'submitdraft',
+            authorization: [{
+              actor: this.walletName,
+              permission: 'active'
+            }],
+            data: {
+              proposer: this.walletName,
+              proposal_name,
+              title,
+              monthly_budget,
+              duration,
+              proposal_json
+            }
+          }]
+        }, { keyProvider: privateKey.key })
+        this.fetch()
+      } catch (error) {
+        console.log('err', error)
+        userError(error.message)
+      }
+    },
+    async canceldraft (proposalName) {
+      const privateKeyEncrypted = JSON.stringify(this.$store.state.currentwallet.wallet.privateKeyEncrypted)
+      const privateKey = this.$configManager.decryptPrivateKey(this.privateKeyPassword, privateKeyEncrypted)
+
+      try {
+        await eos.transact({
+          actions: [{
+            account: 'volentixwork',
+            name: 'canceldraft',
+            authorization: [{
+              actor: this.walletName,
+              permission: 'active'
+            }],
+            data: {
+              proposer: this.walletName,
+              proposal_name: proposalName
+            }
+          }]
+        }, { keyProvider: privateKey.key })
+        this.fetch()
+      } catch (error) {
+        console.log('err', error)
+        userError(error.message)
+      }
+    },
+
+    async activate (proposalName) {
+      const privateKeyEncrypted = JSON.stringify(this.$store.state.currentwallet.wallet.privateKeyEncrypted)
+      const privateKey = this.$configManager.decryptPrivateKey(this.privateKeyPassword, privateKeyEncrypted)
+
+      try {
+        await eos.transact({
+          actions: [{
+            account: 'volentixwork',
+            name: 'activate',
+            authorization: [{
+              actor: this.walletName,
+              permission: 'active'
+            }],
+            data: {
+              proposer: this.walletName,
+              proposal_name: proposalName,
+              start_voting_period: null // Start voting period
+            }
+          }]
+        }, { keyProvider: privateKey.key })
+        this.fetch()
+      } catch (error) {
+        console.log('err', error)
+        userError(error.message)
+      }
+    },
+
+    fetch () {
+      eos.getTable('volentixwork', 'volentixwork', 'proposals').then(r => {
+        this.proposals = r
+      })
+
+      // TODO Here should be current user account
+      // eos.getTable('volentixwork', this.walletName, 'drafts').then(r => {
+      eos.getTable('volentixwork', 'avral.pro', 'drafts').then(r => {
+        this.drafts = r
+      })
+    },
+
     async setupPlatformPath () {
       this.configPath = await platformTools.filePath()
-    },
-    goChangePassword: function () {
-      this.$router.push({ path: '/change-password' })
-    },
-    setNetwork: function () {
-      this.$store.dispatch('settings/toggleNetwork', this.network)
-      this.$q.notify({ message: `Network changed to ${this.network}`, color: 'positive' })
-    },
-    async backupConfig () {
-      try {
-        await configManager.backupConfig()
-        if (this.$q.platform.is.android) {
-          this.$q.notify({ color: 'positive', message: 'Config Saved' })
-        }
-      } catch (e) {
-        // TODO: Exception handling
-      }
     }
   }
 }
 </script>
-<style lang="scss" scoped>
-@import "~@/assets/styles/variables.scss";
+<style lang='scss' scoped>
+@import '~@/assets/styles/variables.scss';
 .standard-content{
   padding: 5% 10%;
   display: flex;
@@ -457,7 +508,7 @@ export default {
   }
 }
 </style>
-<style lang="scss">
+<style lang='scss'>
   .option--avatar{
     border: 1px solid rgba(black, .1);
     width: 35px;
