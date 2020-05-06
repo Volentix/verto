92adc296da5e229b5e2a2721294e358423ce74f6
Gregory Luneau <gregory.luneau@gmail.com>
2020-05-06 08:52:47 -0400

    lowercase only

--- app/src/pages/Verto/Landing.vue
+++ app/src/pages/Verto/Landing.vue
@@ -50,6 +50,7 @@
 
 <script>
 import configManager from '@/util/ConfigManager'
+import { version } from '../../../package.json'
 
 export default {
   name: 'Login',
@@ -66,6 +67,7 @@ export default {
     }
   },
   async mounted () {
+    this.version = version
     this.hasConfig = !!await configManager.hasVertoConfig()
     if (!this.hasConfig) {
       this.$router.push({ name: 'create-password' })
