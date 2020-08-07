855f329c97199bec5b1f55d6d6a4cc28f2a6ba36
bitar youssef <bitardev@gmail.com>
2020-08-07 23:58:05 +0100

    Show the verto id on profile page load

--- app/src/components/Verto/Profile.vue
+++ app/src/components/Verto/Profile.vue
@@ -12,6 +12,7 @@
           <q-item-section class="item-name">{{item.name}}</q-item-section>
           <q-item-section>
             <div class="flex justify-end">
+              <div v-if="item.info === 'Linked'" class="flex flex-center q-mr-md text-grey">{{existingCruxID}}</div>
               <q-btn v-if="item.info === 'Linked'" flat unelevated text-color="grey" @click="copyToClipboard(existingCruxID , 'Verto ID')" round class="btn-copy" icon="o_file_copy" />
             </div>
           </q-item-section>
