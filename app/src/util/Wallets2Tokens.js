80f4a9fb3a522d35c6516ec49d5385abf681ef69
Gregory Luneau <gregory.luneau@gmail.com>
2020-09-25 08:09:11 -0400

    less space

--- app/src/util/Wallets2Tokens.js
+++ app/src/util/Wallets2Tokens.js
@@ -26,7 +26,7 @@ class Wallets2Tokens {
       //   vespucciScore = result.vespucciScore
       //   wallet.vespucciScore = vespucciScore
       // })
-    
+
       if (wallet.type === 'eos') {
         wallet.to = '/verto/wallets/eos/eos/' + wallet.name.toLowerCase()
         wallet.icon = 'https://files.coinswitch.co/public/coins/' + wallet.type.toLowerCase() + '.png'
