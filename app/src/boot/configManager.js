c766ab8e8b83f7e573e5eae7bbd2f8889449e7c4
iberthony <berthony33@gmail.com>
2021-01-04 08:45:55 -0400

    Passing the network url

--- app/src/boot/configManager.js
+++ app/src/boot/configManager.js
@@ -1,12 +1,12 @@
 import configManager from '../util/ConfigManager'
-import vDexNodeConfigManager from '../util/VDexNodeConfigManager'
+import VDexNodeConfigManager from '../util/VDexNodeConfigManager'
 import EosRPC from '../util/EosWrapper'
 import * as utils from '@/util/utils.js'
 import { userError } from '@/util/errorHandler'
 
 export default ({ Vue }) => {
   Vue.prototype.$configManager = configManager
-  Vue.prototype.$vDexNodeConfigManager = vDexNodeConfigManager
+  Vue.prototype.$vDexNodeConfigManager = new VDexNodeConfigManager('http://140.82.56.143:8888')
   Vue.prototype.$rpc = new EosRPC('http://140.82.56.143:8888')
   Vue.prototype.$utils = utils
   Vue.prototype.$userError = userError
