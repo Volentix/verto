fa3b1a2248099af1a9e9322d20be011462bd56ab
iberthony <berthony33@gmail.com>
2021-01-29 09:00:38 -0400

    Remove test element

--- app/src/pages/Verto/DashboardNewUI.vue
+++ app/src/pages/Verto/DashboardNewUI.vue
@@ -6,7 +6,6 @@
         <div class="row">
             <div class="col col-md-3">
                 <div class="wallets-container">
-                <p @click="$emit('showWizardAction')">Emit</p>
                     <profile-header :isMobile="false" class="marg" version="type2222" />
                     <wallets data-title="Interact with your account"  data-intro="Click on an account/token to see all actions you can perform. Click SETP to associate EOS account(s) to account names" :isMobile="false" :showWallets="false" :isWalletsPage="false" :isWalletDetail="false" />
                 </div>
