8785420bd629b56c19dc9650178e2c30c2b00df7
Berthony <berthony@Berthonys-MacBook-Pro.local>
2021-03-10 07:42:59 -0400

    General UI/UX changes

--- app/src/mixins/Formatter.js
+++ app/src/mixins/Formatter.js
@@ -1,7 +1,8 @@
 export default {
   methods: {
     formatNumber (num, decimals = 4) {
-      return parseFloat(num).toFixed(decimals)
+      let value = parseFloat(num).toFixed(decimals)
+      return isNaN(value) ? 0 : value
     },
     formatDefiboxPool (pool) {
       return this.formatNumber(pool.split(' ')[0]) + ' ' + pool.split(' ')[1]
