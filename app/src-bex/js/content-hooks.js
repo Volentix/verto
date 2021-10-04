8dca4585f4f26430fd7e6bb173b86d2b749b8730
kai nagae <muhammadfaraz9736@gmail.com>
2021-10-04 20:02:01 +0500

    working on sync

--- app/src-bex/js/content-hooks.js
+++ app/src-bex/js/content-hooks.js
@@ -1,8 +1,13 @@
 // Hooks added here have a bridge allowing communication between the BEX Content Script and the Quasar Application.
 // More info: https://quasar.dev/quasar-cli/developing-browser-extensions/content-hooks
 
-export default function attachContentHooks (/* bridge */) {
+export default function attachContentHooks (bridge) {
   // Hook into the bridge to listen for events sent from the client BEX.
+  bridge.on('app.save', event => {
+    bridge.send('app.sync', event.data).then((data) => {
+      bridge.send(event.responseKey, data)
+    })
+  })
   /*
   bridge.on('some.event', event => {
     if (event.data.yourProp) {
--- app/src/pages/Verto/bex/StorageSync.vue
+++ app/src/pages/Verto/bex/StorageSync.vue
@@ -1,27 +1,35 @@
 <template>
+  <div>
 
+  </div>
 </template>
 
 <script>
-import store from '../../../store'
+// import store from '../../../store'
 
 export default {
   name: 'StorageSync',
   mounted () {
-    if (this.$isbex) {
-      this.$nextTick(() => {
-        document.querySelector('#preloader').style.display = 'none'
-      })
-    }
-    if (store.state.currentwallet && store.state.currentwallet.loggedIn === true) {
-      this.$router.push({
-        path: '/verto/dashboard'
-      })
-    } else {
-      this.$router.push({
-        name: 'login'
-      })
-    }
+    // console.log(localStorage.getItem('sync_data'))
+    // if (this.$isbex) {
+    //   this.$nextTick(() => {
+    //     document.querySelector('#preloader').style.display = 'none'
+    //   })
+    // }
+    // if (localStorage.getItem('sync_data')) {
+    //   this.$router.push({
+    //     name: 'syncExtensionWallet'
+    //   })
+    // }
+    // if (store.state.currentwallet && store.state.currentwallet.loggedIn === true) {
+    //   this.$router.push({
+    //     path: '/verto/dashboard'
+    //   })
+    // } else {
+    //   this.$router.push({
+    //     name: 'login'
+    //   })
+    // }
   }
 }
 </script>
